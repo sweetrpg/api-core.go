5dda3e6cf2ac699acf89c2bcb808c4440a2696eb
Paul Schifferer <paul@schifferers.net>
2025-05-11 09:23:54 -0700

    Make options tests a suite

--- util/options_test.go
+++ util/options_test.go
@@ -4,86 +4,96 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/sweetrpg/common.go/logging"
 	dbconstants "github.com/sweetrpg/db.go/constants"
 )
 
-func TestValidParams(t *testing.T) {
+type OptionsTestSuite struct {
+	suite.Suite
+}
+
+func (suite *OptionsTestSuite) TestValidParams() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=1&page[limit]=5"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 1, params.Start)
-	assert.EqualValues(t, 5, params.Limit)
+	assert.EqualValues(suite.T(), 1, params.Start)
+	assert.EqualValues(suite.T(), 5, params.Limit)
 }
 
-func TestNoParams(t *testing.T) {
+func (suite *OptionsTestSuite) TestNoParams() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 0, params.Start)
-	assert.EqualValues(t, dbconstants.QueryDefaultSize, params.Limit)
+	assert.EqualValues(suite.T(), 0, params.Start)
+	assert.EqualValues(suite.T(), dbconstants.QueryDefaultSize, params.Limit)
 }
 
-func TestLowStart(t *testing.T) {
+func (suite *OptionsTestSuite) TestLowStart() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=-5&page[limit]=5"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 0, params.Start)
-	assert.EqualValues(t, 5, params.Limit)
+	assert.EqualValues(suite.T(), 0, params.Start)
+	assert.EqualValues(suite.T(), 5, params.Limit)
 }
 
-func TestOnlyStart(t *testing.T) {
+func (suite *OptionsTestSuite) TestOnlyStart() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=1"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 1, params.Start)
-	assert.EqualValues(t, dbconstants.QueryDefaultSize, params.Limit)
+	assert.EqualValues(suite.T(), 1, params.Start)
+	assert.EqualValues(suite.T(), dbconstants.QueryDefaultSize, params.Limit)
 }
 
-func TestLowLimit(t *testing.T) {
+func (suite *OptionsTestSuite) TestLowLimit() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=1&page[limit]=-1"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 1, params.Start)
-	assert.EqualValues(t, 1, params.Limit)
+	assert.EqualValues(suite.T(), 1, params.Start)
+	assert.EqualValues(suite.T(), 1, params.Limit)
 }
 
-func TestZeroLimit(t *testing.T) {
+func (suite *OptionsTestSuite) TestZeroLimit() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=1&page[limit]=0"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 1, params.Start)
-	assert.EqualValues(t, dbconstants.QueryDefaultSize, params.Limit)
+	assert.EqualValues(suite.T(), 1, params.Start)
+	assert.EqualValues(suite.T(), dbconstants.QueryDefaultSize, params.Limit)
 }
 
-func TestHighLimit(t *testing.T) {
+func (suite *OptionsTestSuite) TestHighLimit() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[start]=1&page[limit]=500"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 1, params.Start)
-	assert.EqualValues(t, dbconstants.QueryMaxSize, params.Limit)
+	assert.EqualValues(suite.T(), 1, params.Start)
+	assert.EqualValues(suite.T(), dbconstants.QueryMaxSize, params.Limit)
 }
 
-func TestOnlyLImit(t *testing.T) {
+func (suite *OptionsTestSuite) TestOnlyLImit() {
 	logging.Init()
 
 	query := "http://localhost:1234/endpoint?page[limit]=5"
 
 	params := GetQueryParams(query)
-	assert.EqualValues(t, 0, params.Start)
-	assert.EqualValues(t, 5, params.Limit)
+	assert.EqualValues(suite.T(), 0, params.Start)
+	assert.EqualValues(suite.T(), 5, params.Limit)
+}
+
+func TestOptionsTestSuite(t *testing.T) {
+	suite.Run(t, new(OptionsTestSuite))
 }
