6b667af9afbd0446651c6a6786a1301182d44d47
Paul Schifferer <paul@schifferers.net>
2025-05-11 09:23:22 -0700

    Add logging for convert function

--- util/convert.go
+++ util/convert.go
@@ -3,21 +3,27 @@ package util
 import (
 	"unsafe"
 
+	"github.com/sweetrpg/common.go/logging"
+
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func ConvertQueryParams(params QueryParams) (filter bson.D, sort bson.D, projection bson.D) {
+	logging.Logger.Debug("Converting query params to BSON", "params", params)
 
 	for _, v := range params.Filter {
-		filter = append(filter, bson.E{v.Field, v.Operation})
+		logging.Logger.Debug("Processing filter", "field", v.Field, "operation", v.Operation)
+		filter = append(filter, bson.E{Key: v.Field, Value: v.Operation})
 	}
 
 	for _, v := range params.Sort {
-		sort = append(sort, bson.E{v.Field, v.Order})
+		logging.Logger.Debug("Processing sort", "field", v.Field, "order", v.Order)
+		sort = append(sort, bson.E{Key: v.Field, Value: v.Order})
 	}
 
 	for _, v := range params.Projection {
-		projection = append(projection, bson.E{v.Field, *(*int)(unsafe.Pointer(&v.Inclusion)) & 1})
+		logging.Logger.Debug("Processing projection", "field", v.Field, "inclusion", v.Inclusion)
+		projection = append(projection, bson.E{Key: v.Field, Value: *(*int)(unsafe.Pointer(&v.Inclusion)) & 1})
 	}
 
 	return
